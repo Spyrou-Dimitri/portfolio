a0a5e6b9176acdfe6bb06d4f6c9dee3ac6c68364
Dimitri Spyrou <dimitrispyrou@Macbook-du-nain.local>
2025-03-22 12:11:32 +0100

    nav + burger for phone almost finish

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -8,35 +8,40 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="<?= pf_asset('css/main.css')?>">
+    <link rel="stylesheet" href="<?= pf_asset('css/main.css') ?>">
     <title><?= get_bloginfo('title') ?></title>
 </head>
 <body>
 <header>
-    <h1>
+    <h1 class="sro">
         <?= get_the_title('58') ?>
     </h1>
-    <nav class="nav">
-        <h2 class="nav__title hidden">
-            <?= wp_get_nav_menu_name("header_menu") ?>
-        </h2>
-        <a class="nav__link" href="#" title="Vers l'accueil">Dimitri S.</a>
-        <ul class="nav__container">
-            <?php foreach (pf_get_navigation_links('header_menu') as $link): ?>
-                <li class="nav__container__items">
-                    <a href="<?= $link->url ?>" class="nav__container__items__link" title="Vers la page <?= $link->label?>"><?= $link->label?></a>
-                </li>
-            <?php endforeach; ?>
+    <div class="header__containner">
+        <nav class="nav">
+            <h2 class="nav__title sro">
+                <?= wp_get_nav_menu_name("header_menu") ?>
+            </h2>
+            <a class="nav__link" href="#" title="Vers l'accueil">Dimitri S.</a>
+            <input type="checkbox" name="burger" id="burger__button">
+            <label for="burger__button" class="sro">Menu dépliant</label>
+            <div class="burger__wrapper">
+                <span class="burger__wrapper__lines up"></span>
+                <span class="burger__wrapper__lines middle"></span>
+                <span class="burger__wrapper__lines down"></span>
+            </div>
+            <ul class="nav__container">
+                <?php foreach (pf_get_navigation_links('header_menu') as $link): ?>
+                    <li class="nav__container__items">
+                        <a href="<?= $link->url ?>" class="nav__container__items__link"
+                           title="Vers la page <?= $link->label ?>"><?= $link->label ?></a>
+                    </li>
+                <?php endforeach; ?>
 
-        </ul>
-        <label for="burger">Menu dépliant</label>
-        <div>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <input type="checkbox" name="burger" id="burger">
-    </nav>
+            </ul>
+
+        </nav>
+    </div>
+
 </header>
 <main>
 
