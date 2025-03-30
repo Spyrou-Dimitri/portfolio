e122ccf3376318f44ccc16e567ccdf77c494a0ac
Dimitri Spyrou <dimitrispyrou@Macbook-du-nain.local>
2025-03-31 01:19:38 +0200

    about almost finish

--- wp-content/themes/portfolio/template-about.php
+++ wp-content/themes/portfolio/template-about.php
@@ -2,74 +2,89 @@
 get_header();
 
 ?>
-<section class="about">
-    <?php if (have_posts()): while (have_posts()):
-    the_post(); ?>
-    <h2 class="about__title">
-        <?= get_field('about-title'); ?>
-    </h2>
-    <article class="about__presentation">
-        <h3 class="about__presentation">
-            <?= get_field('presentation-title'); ?>
-
-        </h3>
-        <p class="about__title">
-            <?= get_field('description'); ?>
-        </p>
-        <div class="about__container">
-            <img src="<?= get_field('about-img'); ?>" alt="Photo de moi" class="about__container__img">
-        </div>
-    </article>
-</section>
-<section class="career">
-    <h2 class="career title">
-        Mon parcours
-    </h2>
-    <?php if (have_rows('career')) : ?>
-        <?php while (have_rows('career')) : the_row(); ?>
-            <section>
-                <h3><?= get_sub_field('course-name') ?></h3>
-                <p><?= get_sub_field('course-time') ?></p>
-            </section>
-        <?php endwhile; ?>
-    <?php else : ?>
-        <p>Aucune donnée trouvée.</p>
-    <?php endif; ?>
-        </section>
-</section>
-<div class="bgWayOfWorking" >
-    <section>
-        <h2>
+<div class="bgAbout">
+    <section class="about">
+        <?php if (have_posts()): while (have_posts()):
+        the_post(); ?>
+        <h2 class="about__title">
+            <?= get_field('about-title'); ?>
         </h2>
-            <?php if (have_rows('step-list')) : ?>
-                <?php while (have_rows('step-list')) : the_row(); ?>
-                    <section>
-                        <img src="" alt="">
-                        <h3><?= get_sub_field('step-title') ?></h3>
-                        <p><?= get_sub_field('step-desc') ?></p>
-                    </section>
-                <?php endwhile; ?>
-            <?php else : ?>
-                <p>Aucune donnée trouvée.</p>
-            <?php endif; ?>
+        <article class="about__presentation">
+            <div class="about__presentation__content">
+                <h3 class="about__presentation__content__title">
+                    <?= get_field('presentation-title'); ?>
+                </h3>
+                <p class="about__presentation__content__paragraph">
+                    <?= get_field('description'); ?>
+                </p>
+            </div>
+            <div class="about__presentation__img">
+                <img src="<?= get_field('about-img'); ?>" alt="Photo de moi">
+            </div>
+        </article>
     </section>
 </div>
-<section>
-    <h2>
-        <?= get_field('tools-title') ?>
-    </h2>
-    <ul>
-        <?php if (have_rows('tools-list')): while (have_rows('tools-list')): the_row(); ?>
-        <?php
-            $img = get_sub_field('tool-item');
-            if ($img) : ?>
-        <li>
-            <img src="<?= $img['url']; ?>" alt="<?= $img['alt']; ?>" width="5%">
-        </li>
-        <?php endif;?>
-        <?php endwhile; endif; ?>
-    </ul>
-</section>
+    <section class="career">
+        <h2 class="career__title">
+            Mon parcours
+        </h2>
+        <div class="career__container">
+            <div class="career__container__img">
+                <img src="/wp-content/themes/portfolio/resources/img/ball8.svg" alt="Boule 8 du billard">
+            </div>
+            <ul class="career__container__list">
+                <?php if (have_rows('career')) : ?>
+                    <?php while (have_rows('career')) : the_row(); ?>
+                        <li class="career__container__list__items">
+                            <time class="career__container__list__items__date"><?= get_sub_field('course-time') ?></time>
+                            <h3 class="career__container__list__items__title"><?= get_sub_field('course-name') ?></h3>
+                        </li>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <p>Aucune donnée trouvée.</p>
+                <?php endif; ?>
+            </ul>
+        </div>
+
+    </section>
+    <div class="bgWayOfWorking">
+        <section class="step">
+            <h2 class="step__title">
+                <?= get_field("wayOfWorking-title") ?>
+            </h2>
+            <ul class="step__list">
+                <?php if (have_rows('wayOfWorking-list')) : ?>
+                    <?php while (have_rows('wayOfWorking-list')) : the_row(); ?>
+
+                        <li class="step__list__items">
+                            <img class="step__list__items__img" src="<?= get_sub_field('wayOfWorking-ball') ?>" alt="<?= get_sub_field('ball-alt') ?>">
+                            <h3 class="step__list__items__title"><?= get_sub_field('wayOfWorking-titleStep') ?></h3>
+                            <p class="step__list__items__paragraph"><?= get_sub_field('wayOfWorking-desc') ?></p>
+                        </li>
+
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <p>Aucune donnée trouvée.</p>
+                <?php endif; ?>
+            </ul>
+        </section>
+    </div>
+    <section class="tools">
+        <h2 class="tools__title">
+            <?= get_field('tools-title') ?>
+        </h2>
+        <ul class="tools__list">
+            <?php if (have_rows('tools-list')): while (have_rows('tools-list')): the_row(); ?>
+                <?php
+                $img = get_sub_field('tool-item');
+                if ($img) : ?>
+                    <li class="tools__list__items">
+                        <img class="tools__list__items__img" src="<?= $img['url']; ?>" alt="<?= $img['alt']; ?>">
+                    </li>
+                <?php endif; ?>
+            <?php endwhile; endif; ?>
+        </ul>
+    </section>
 <?php endwhile;
 endif; ?>
 <?php
